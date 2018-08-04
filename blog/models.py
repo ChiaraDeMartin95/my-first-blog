f1772dbfa95092b5f2428bb7c977595fd6bf18b2
chiarademartin <chiara.demartin.keira@gmail.com>
2018-08-04 16:02:21 +0200

    modifiche leggere ~

--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from django.utils import timezone
 
-
+#from, import servono per importare pezzi di un altro file in questo stesso file
 class Post(models.Model):
-    author = models.ForeignKey('auth.User')
-    title = models.CharField(max_length=200)
-    text = models.TextField()
+    # Con Class creiamo il nostro oggetto, che si chiama Post (p maiuscola).
+    # Il pezzo in parentesi indica che si tratta di un modello Django, che dovrà essere salvato nel suo database
+    author = models.ForeignKey('auth.User')  #link ad un altro modello
+    title = models.CharField(max_length=200) #definisco testo con numero limitato di caratteri
+    text = models.TextField()                #definisco testo con numero illimitato di caratteri
     created_date = models.DateTimeField(
         default=timezone.now
     )
@@ -14,9 +16,9 @@ class Post(models.Model):
         null=True
     )
 
-    def publish(self):
+    def publish(self):   #def indica che si tratta di una funzione/metodo e publish è il nome del metodo
         self.published_date = timezone.now()
         self.save()
 
-    def __str__(self):
+    def __str__(self):   #questo metodo mi ritorna il titolo del post
         return self.title
